166be810e3eed4cc18dc597c5e63281432151a6b
DeltaFXD <32136483+DeltaFXD@users.noreply.github.com>
2020-10-06 16:53:07 +0200

    More init

---
+++ src/Main.cpp
@@ -0,0 +1,9 @@
+#include <Windows.h>
+#pragma comment(lib,"d3d11.lib")
+#pragma comment(lib,"DirectXTK12.lib")
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow) {
+	
+	
+	return 0;
+}
